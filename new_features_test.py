cad8b2a2bd67b55ed5ef0950f1eb6afe26726be4
Qaiser Abbas <mqaiser617@gmail.com>
2023-04-06 11:58:52 +0500

    Update new_features_test.py
    Removed redundant function code. Now single function is used for both videos (local and online youtube).

--- new_features_test.py
+++ new_features_test.py
@@ -41,71 +41,11 @@ args = parser.parse_args()
 video_path = args.video_path
 
 
-def youtube_video_file(video_path):
+def start_streaming(stream_object):
     default_path = (
         args.destination
     )  # "D:\\github codes\\test2\\" ==> \\ must be used with \\ in the end as well
-    stream = CamGear(
-        source=video_path,
-        stream_mode=True,
-        time_delay=1,
-        logging=True,
-    ).start()
-    currentframe = 0
-    while True:
-
-        frame = stream.read()  # using functions from vidGear module
-        if frame is None:
-            break
-
-        # cv2.imshow("Output Frame", frame)  # optional if u want to show the frames
-
-        # name = path + str(currentframe) + ".jpg" # test/0.jpg
-        # print("Reading..." + name)
-        temp_img = "temp.png"
-        cv2.imwrite(temp_img, frame)
-        print(f"Classifying image {temp_img}")
-        class_name = classify_image(temp_img)
-        # print(type(class_name))
-        print(
-            f"#############==> Predicted Class Name: {class_name} <==#########################"
-        )
-        print(f"Removing temporary image {class_name}")
-        os.remove(temp_img)
-
-        if not os.path.exists(default_path + str(class_name)):
-            print(f"Making dir {class_name}")
-            os.makedirs(default_path + class_name)
-            # path = path + str(class_name)
-            print(f"Entering into {default_path + class_name}")
-            # default_path=default_path+str(class_name)
-            os.chdir(default_path + class_name)
-            print(f"Writing frames to dir {default_path + class_name}")
-            name = str(class_name) + str(currentframe) + ".jpg"  # test/labrador/0.jpg
-            cv2.imwrite(name, frame)
-        elif os.path.exists(default_path + str(class_name)):
-            # default_path=default_path+str(class_name)
-            os.chdir(default_path + class_name)
-            name = str(class_name) + str(currentframe) + ".jpg"
-            cv2.imwrite(name, frame)
-        else:
-            pass
-
-        # cv2.imwrite(name, frame)
-        currentframe += 30  ##chnage 5 with the number of frames. Here 5 means capture frame after every 5 frames
-        # usually videos are 30fps so if here 30 is provided a frame will be captures after every second.
-
-        key = cv2.waitKey(1) & 0xFF
-
-        if key == ord("q"):
-            break
-
-
-def local_video_file(video_path):
-    default_path = (
-        args.destination
-    )  # "D:\\github codes\\test2\\" ==> \\ must be used with \\ in the end as well
-    stream = VideoGear(source=video_path).start()
+    stream = stream_object
     currentframe = 0
     while True:
 
@@ -177,11 +117,18 @@ if __name__ == "__main__":
         if args.video_path[-4:] in allowed_video_format:
             print(f"Found valid video file with {args.video_path[-4:]} format.")
             # load_classes_and_model()
-            local_video_file(video_path)
+            stream_object = VideoGear(source=video_path).start()
+            # local_video_file(video_path)
+            start_streaming(stream_object)
         elif args.video_path[:4] == "http" and "youtube.com" in args.video_path:
             print("File found with youtube video link")
-            # load_classes_and_model()
-            youtube_video_file(video_path)
+            stream_object = CamGear(
+                source=video_path,
+                stream_mode=True,
+                time_delay=1,
+                logging=True,
+            ).start()
+            start_streaming(stream_object)
         else:
             raise ValueError(
                 "Please provide a valid video file path or youtube video link."
